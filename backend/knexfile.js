3df0c655bcb5966a7864ffd130df67a281709ed1
bacamargo <bpcamargo9@hotmail.com>
2023-04-18 17:49:53 -0300

    Improving security

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,13 +1,9 @@
-const db = require('./.env')
+const { db } = require('./.env')
 
 // configuração para acessar o banco do postgresql
 module.exports = {
     client: 'postgresql',
-    connection: {
-      database: 'Users',
-      user:     'postgres',
-      password: 'admin'
-    },
+    connection: db,
     pool: {
       min: 2,
       max: 10
