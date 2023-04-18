b7c56bd5c0075155b0b37adcff5bb0b387519bd2
bacamargo <bpcamargo9@hotmail.com>
2023-04-18 16:59:25 -0300

    Validating users as admin

---
+++ backend/config/admin.js
@@ -0,0 +1,10 @@
+// checar se o usuário é um administrador
+module.exports = middleware => {
+    return (req, res, next) => {
+        if(req.user.admin) {
+            middleware(req, res, next)
+        } else {
+            res.status(401).send('User is not admin')
+        }
+    }
+}
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = app => {
     // rotas de autenticação
     app.post('/signup', app.api.user.save)
@@ -7,11 +9,11 @@ module.exports = app => {
     // rotas para requisição para salvar e adicionar novo usuário, atualizar e buscar (ler)
     app.route('/users')
         .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
 }
