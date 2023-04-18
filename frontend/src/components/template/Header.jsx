200c8c8a4438ada51b390a1c6d110fc3a27f89f0
bacamargo <bpcamargo9@hotmail.com>
2023-04-18 20:07:07 -0300

    Starting frontend configs

---
+++ frontend/src/components/template/Header.jsx
@@ -0,0 +1,7 @@
+import './Header.css'
+import React from 'react'
+
+export default properties =>
+    <header className="header">
+        Header
+    </header>
---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,7 @@
+import './Nav.css'
+import React from 'react'
+
+export default properties =>
+    <aside className='menu-area'>
+        Navigation
+    </aside>
