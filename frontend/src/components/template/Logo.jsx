4b2d27787d6b3ea95897b88f29c3f8713f7d9263
bacamargo <bpcamargo9@hotmail.com>
2023-04-18 22:48:26 -0300

    Styling components and logo component

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assets/imgs/Kosmic.png'
 import React from 'react'
 
 export default properties =>
     <aside className='Logo'>
-        Logo
+        <a href='/' className='logo'>
+            <img src={logo} alt='logo' />
+        </a>
     </aside>
